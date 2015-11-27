b1f045e5a5b7c890bc7a52c1cc98ae3f224a465b
alexlerikos <alerikos@gmail.com>
2015-11-27 02:45:02 -0800

    Added default constructor for CMechanism class -Added default beam lengths, phase angles and unit type :

--- mechanism.h
+++ mechanism.h
@@ -3,8 +3,8 @@
  *            header file for class CMechanism
  ************************************************************/
 
-#ifndef _QUICKRETURN_H_
-#define _QUICKRETURN_H_
+#ifndef _MECHANISM_H_
+#define _MECHANISM_H_
 
 #include <linkage.h>
 #include <stdio.h>
@@ -25,14 +25,37 @@ class CMechanism
 	public:
 
 	//public attributes
+
+
+
 	
-	//public member functions	
+
+	// Constructor/Destructor
+    CMechanism();
+    ~CMechanism();
+
+
+	//public member functions
+	void uscUnit(bool unit);
+    void setLinks();
+    double position();
+    double velocity();
+    double acceleration();
+    void plotSliderPos();
+    void plotSliderVel();
+    void animation();
 
 
 	private:
 
 	//private attributes
+	bool m_uscunit;          // unit choice
+
+	double r_length[1:8]; // array containing length of members 1:8
+	double theta[1:5]; //array containing beam phase angles 1:5
 
 
 	//private member functions	
+
+
 }
