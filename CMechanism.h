6f797cdf9305206783197b78fa4faf2c3efb2886
GuihermeOtto <gui.potto@gmail.com>
2015-12-03 16:52:10 -0800

    Add my files, basic functions

---
+++ CMechanism.h
@@ -0,0 +1,65 @@
+/* Testing Program */
+
+#include "mechanism.h"
+
+int main()
+{
+   class CMechanism cm;
+   double output;
+
+   bool unit = SI;
+   double r1 = 5.0, 
+          r2 = 1.0,     
+          r3 = 3.0, 
+          r4 = 4.5, 
+          r5 = 8.0,
+          r7 = 3.0,
+          rp = 2.0;
+   
+   double theta1 = deg2rad( 10);
+   
+   double theta2 = deg2rad(-30);
+   double omega2 = 15.0;
+   double alpha2 = 0;
+   
+   double beta = deg2rad(30);
+
+   cm.uscUnit(unit);
+   cm.setLinks(r1, r2, r3, r4, r5, r7, rp, theta1, beta);
+
+/* Calculations */
+
+    printf("Theta 1 = %f (rad)\n", cm.AngPosition(theta2, LINK_1));   
+    printf("Theta 2 = %f (rad)\n", cm.AngPosition(theta2, LINK_2));
+    printf("Theta 3 = %f (rad)\n", cm.AngPosition(theta2, LINK_3));
+    printf("Theta 4 = %f (rad)\n", cm.AngPosition(theta2, LINK_4));
+    printf("Theta 5 = %f (rad)\n\n", cm.AngPosition(theta2, LINK_5));
+    
+    printf("Point B = %f \n", cm.Point(theta2, omega2, alpha2, POS, POINT_B));
+    printf("Point C = %f \n", cm.Point(theta2, omega2, alpha2, POS, POINT_C));
+    printf("Point P = %f \n\n", cm.Point(theta2, omega2, alpha2, POS, POINT_P));
+    
+    printf("Omega 1 = %f (rad/s)\n", cm.AngVelocity(theta2, omega2, LINK_1));   
+    printf("Omega 2 = %f (rad/s)\n", cm.AngVelocity(theta2, omega2, LINK_2));
+    printf("Omega 3 = %f (rad/s)\n", cm.AngVelocity(theta2, omega2, LINK_3));
+    printf("Omega 4 = %f (rad/s)\n", cm.AngVelocity(theta2, omega2, LINK_4));    
+    printf("Omega 5 = %f (rad/s)\n\n", cm.AngVelocity(theta2, omega2, LINK_5)); 
+    
+    printf("Point B = %f \n", cm.Point(theta2, omega2, alpha2, VEL, POINT_B));
+    printf("Point C = %f \n", cm.Point(theta2, omega2, alpha2, VEL, POINT_C));
+    printf("Point P = %f \n\n", cm.Point(theta2, omega2, alpha2, VEL, POINT_P));
+
+    printf("Alpha 1 = %f (rad/s^2)\n", cm.AngAccel(theta2, omega2, alpha2, LINK_1));   
+    printf("Alpha 2 = %f (rad/s^2)\n", cm.AngAccel(theta2, omega2, alpha2, LINK_2));
+    printf("Alpha 3 = %f (rad/s^2)\n", cm.AngAccel(theta2, omega2, alpha2, LINK_3));
+    printf("Alpha 4 = %f (rad/s^2)\n", cm.AngAccel(theta2, omega2, alpha2, LINK_4));  
+    printf("Alpha 5 = %f (rad/s^2)\n\n", cm.AngAccel(theta2, omega2, alpha2, LINK_5));
+    
+    printf("Point B = %f \n", cm.Point(theta2, omega2, alpha2, ACCEL, POINT_B));
+    printf("Point C = %f \n", cm.Point(theta2, omega2, alpha2, ACCEL, POINT_C));
+    printf("Point P = %f \n\n", cm.Point(theta2, omega2, alpha2, ACCEL, POINT_P));
+    
+    printf("Done!\n");
+
+    return 0;
+}
--- mechanism.h
+++ mechanism.h
@@ -1,10 +1,5 @@
-/************************************************************
- * File name: mechanism.h
- *            header file for class CMechanism
- ************************************************************/
-
-#ifndef _MECHANISM_H_
-#define _MECHANISM_H_
+#ifndef _mechanism_h_
+#define _mechanism_h_
 
 #include <linkage.h>
 #include <stdio.h>
@@ -14,6 +9,23 @@
 #include <complex.h>
 #include <chplot.h>
 
+enum 
+{
+   SI,             //  SI units, use with uscUnit()
+   USC,            //  USC units,  "    "    "
+   LINK_1,         //  Link 2
+   LINK_2,         //  Link 2
+   LINK_3,         //  Link 3
+   LINK_4,         //  Link 4
+   LINK_5,         //  Link 5
+   POINT_B,        //  Point C 
+   POINT_C,        //  Point C 
+   POINT_P,        //  Point C 
+   POS,
+   VEL,
+   ACCEL,
+};
+
 
 /**********************************************
  * CMechanism class definition
@@ -21,41 +33,45 @@
 
 class CMechanism
 {
+    private:
+        // Private data members
+        bool m_uscunit;          // unit choice
 
-	public:
+        double m_r[1:8];         // lengths of links
+        double m_theta[1:8];     // phase angles for the links
+        double m_omega[1:8];
+        double m_alpha[1:8];
 
-	//public attributes
-
-
-
-	
-
-	// Constructor/Destructor
-    CMechanism();
-    ~CMechanism();
-
-
-	//public member functions
-	void uscUnit(bool unit);
-    void setLinks();
-    double position();
-    double velocity();
-    double acceleration();
-    void plotSliderPos();
-    void plotSliderVel();
-    void animation();
+        double m_rp, m_beta;
+        double complex Bp, Cp, Pp;
+        double complex Bv, Cv, Pv;
+        double complex Ba, Ca, Pa;
+    
+        // Private function members 
+        void m_initialize(void);              // initialize private members
+        void calcPosition(double theta2);     // calc. ang. pos. and m_r9
+        void calcVelocity(double theta2, omega2);
+        void calcAccel(double theta2, omega2, alpha2);
 
 
-	private:
+    public:
+        // Constructor and Destructor
+        CMechanism();
+        ~CMechanism();
 
-	//private attributes
-	bool m_uscunit;          // unit choice
+        // Setting information functions
+        void setLinks(double r1, r2, r3, r4, r5, r7,rp, theta1, beta);
+        void uscUnit(bool unit); 
 
-	double r_length[1:8]; // array containing length of members 1:8
-	double theta[1:5]; //array containing beam phase angles 1:5
+        // Information for specified point or link
+        double AngPosition(double theta2, int link);
+        double AngVelocity(double theta2, omega2, int link);
+        double AngAccel(double theta2, omega2, alpha2, int link);
+        double complex Point(double theta2, omega2, alpha2, int prop, link);
+    
 
+};
 
-	//private member functions	
+#pragma importf <CMechanism.chf>
 
-
-}
+#endif
